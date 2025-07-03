d40ef104fe684fb94829c47fbb630dea92c5ce87
Syed Sohail <74762045+SyedSohail786@users.noreply.github.com>
2025-07-03 15:34:48 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -7,10 +7,7 @@ const cookieParser = require('cookie-parser');
 const PORT = process.env.PORT || 5000;
 const app = express();
 // Middleware
-app.use(cors({
-  origin: process.env.FRONTEND_URL,
-  credentials: true
-}));
+app.use(cors());
 
 app.use(express.json());
 
